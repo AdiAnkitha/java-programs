6a04d8f79dccf0cd098e47b4738c22cb6657f0be
AdiAnkitha <adiankithaankitha@gmail.com>
2025-04-17 21:07:59 +0530

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,15 @@
+public class datatypes {
+ public static void main(String[] args) {
+    int a = 5;
+    double b = 2.5;
+    char c = 'A';
+    boolean d = true;
+    String e = "hello";
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+
+ }   
+}
