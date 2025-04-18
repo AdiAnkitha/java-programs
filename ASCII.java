062840a0ea7f29b3356b12d8deb0031debaab60f
AdiAnkitha <adiankithaankitha@gmail.com>
2025-04-18 19:21:43 +0530

    Create ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,10 @@
+public class ASCIIvalues {
+    public static void main(String[] args) {
+        char ch ='a';
+        char ca ='m' ;
+        int ASCIIvalue1 = ch;
+        int ASCIIvalue2 = ca;
+        System.out.println("the ASCIIvalue of"+ ch +"is"+ ASCIIvalue1);
+        System.out.println("the ASCIIvalue of"+ ca +"is"+ ASCIIvalue2);
+    }
+}
