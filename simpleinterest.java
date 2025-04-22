b0153a8623b3abac4f874d141e4042896729e9f9
AdiAnkitha <adiankithaankitha@gmail.com>
2025-04-22 06:32:34 +0530

    Create simpleinterest.java

---
+++ simpleinterest.java
@@ -0,0 +1,7 @@
+public class program21 {
+  public static void main(String[]args){
+    int p = 20000, r = 20, t = 5;
+    int s = (p * t * r) / 100;
+ System.out.println("simple interest ="+s);
+  }  
+}
