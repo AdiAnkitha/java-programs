9e78c8f9059a104c015e8d86d2c10fdee9bcc8c1
AdiAnkitha <adiankithaankitha@gmail.com>
2025-04-28 07:28:34 +0530

    Create invertedpattern.java

---
+++ invertedpattern.java
@@ -0,0 +1,16 @@
+public class patteern7 {
+    public static void main(String args[]){
+        int n = 5;
+        for(int j = 0; j < n; j++)
+        {
+           for (int i = n; i > j; i--){
+           
+           System.out.print("*");
+
+            }
+            System.out.println();
+        }
+        
+        
+    }
+}
